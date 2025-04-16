d08f2db8ea15ca90d5c9282e79a4a96fe385e1c2
mjid13 <abdulmajeed.alhadhrami@gmail.com>
2025-04-16 16:52:29 +0400

    adding batter logic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data_extractor.get_exact_pg import PortalClient
 import asyncio
-from data_transform.core_utils import extract_pdf_data, save_text_to_csv, generate_csv_from_docs
+from data_transform.core_utils import extract_pdf_data, save_text_to_csv, generate_csv_from_docs, delete_pdf
 import logging
 
 
@@ -8,7 +8,54 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-async def main(username: str, password: str, accounts_list: list[str], output_directory: str):
+def get_user(user_type):
+    """
+    Returns the username and password based on the user type.
+
+    Args:
+        user_type (str): The type of user (e.g., "MAZOON", "MAJAN", "TANVEER", "MUSCAT").
+
+    Returns:
+        tuple: A tuple containing the username and password.
+    """
+    users = {
+        "MAZOON": {
+            "username": "emzec",
+            "password": "emzec"
+        },
+        "MAJAN": {
+            "username": "oneic_mjec",
+            "password": "oman1234"
+        },
+        "TANVEER": {
+            "username": "oneic",
+            "password": "123456"
+        },
+        "MUSCAT": {
+            "username": "ONIEC1",
+            "password": "ONIEC1"
+        }
+    }
+    if user_type in users:
+        return users[user_type.upper()]["username"], users[user_type.upper()]["password"]
+    else:
+        logger.error(f"User type '{user_type}' not recognized. Defaulting to MUSCAT.")
+        return users["MUSCAT"]["username"], users["MUSCAT"]["password"]
+
+
+
+
+async def main(user_type: str, accounts_list: list[str], output_directory: str):
+    """
+    Main function to handle the workflow of fetching and processing documents.
+
+    Args:
+        user_type (str): The user_type for authentication.
+        accounts_list (list[str]): List of account numbers to process.
+        output_directory (str): Directory to save the output files.
+    """
+    # Initialize the PortalClient with the provided username and password
+    username, password = get_user(user_type)
     client = PortalClient(username=username, password=password, cookies=[])
     async with client:
         # Step 1: Login
@@ -53,15 +100,20 @@ async def main(username: str, password: str, accounts_list: list[str], output_di
                 # Step 8: Fetch PDF data for the document and save it
                 doc_id = document.get("Id")
                 pdf_data = await client.fetch_pdf_data(document_id=doc_id)
-                await client.save_pdf(pdf_data, filepath=f'{doc_id}.pdf')
+                await client.save_pdf(pdf_data, filepath=f'./data_extractor/{doc_id}.pdf')
                 logger.info(f"PDF saved successfully for document ID {doc_id}.")
 
 
                 # Step 9: Extract text from the PDF
-                extracted_data = await extract_pdf_data(f'{doc_id}.pdf')
+                extracted_data =  extract_pdf_data(f'./data_extractor/{doc_id}.pdf')
 
                 # Step 10: Save extracted text to CSV TODO: here we need to insert the data to the file if it is already exist
-                csv_data = save_text_to_csv(output_directory, extracted_data)
+                save_text_to_csv(output_directory, extracted_data)
+                logger.info(f"Data saved successfully for account number {account_no}.")
+                delete_pdf(f'./data_extractor/{doc_id}.pdf')
+                logger.info(f"PDF deleted successfully for document ID {doc_id}.")
+
+
 
 
 
@@ -69,3 +121,12 @@ async def main(username: str, password: str, accounts_list: list[str], output_di
                 logger.error(f"An error occurred: {e}")
                 continue
 
+
+if __name__ == "__main__":
+    # Example usage
+    user_type = "MAZOON"  # Replace with the desired user type
+    accounts_list = ["02341583", "18061449"]  # Replace with actual account numbers
+    output_directory = "output"  # Replace with the desired output directory
+
+    # Run the main function
+    asyncio.run(main(user_type, accounts_list, output_directory))
